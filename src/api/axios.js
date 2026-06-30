d3a42e416329de0f2b661e7c8c0e0b1ff9eac860
0559101089r-coder <0559101089r@gmail.com>
2026-06-30 20:16:15 +0600

    api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const requester = axios.create({
-  baseURL: 'https://teplovikkg.com/api/v1/',
+  baseURL: 'https://teplovik.kg/api/v1/',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
